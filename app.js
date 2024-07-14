496024870a74ef429ba923fb1d43bec85a8d396c
Mike Pridik <git@formsatz.de>
2024-07-14 18:33:23 +0200

    init hackathon from kc

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Let's go der Hase!
+    console.log('%cmade with ♥ Mike Pridik – formsatz.de', 'background: rgb(43,84,124); color:#fff;padding: 3px');
+
+	
+});
